d8b89cfcb38f5535ec23015bf3e3c5b16e045e96
Marco Becattini <marco.becattini@outlook.com>
2021-03-11 00:33:39 +0100

    Mobile carousel animation

--- resources/js/components/FormComponent.vue
+++ resources/js/components/FormComponent.vue
@@ -1,142 +1,131 @@
 <template>
     <div>
         <div class="columns">
-            <div class="column is-6 is-offset-3">
-                <div class="box">
+            <div class="column is-full carousel-container is-clipped">
+                <div class="is-relative">
 
-                    <div class="columns">
-                        <div class="column is-full">
-                            <h1 class="title is-4 has-text-primary has-text-centered">
-                                3 Steps Form
-                            </h1>
-                        </div>
-                    </div>
+                    <b-loading :is-full-page="false" v-model="loading"></b-loading>
 
-                    <div class="columns">
-                        <div class="column is-full p-0">
-                            <h3 class="subtitle is-6 has-text-grey-darker has-text-centered p-0">
-                                Practice Test | Treedom
-                            </h3>
-                        </div>
-                    </div>
+                    <transition :name="transitionType" mode="in-out">
+                        <div v-if="!loading">
+                            <div class="columns">
+                                <div class="column is-full">
+                                    <ul class="steps">
+                                        <li
+                                            class="step-item"
+                                            :class="{ 'is-completed': formData.step > 1, 'is-active': formData.step == 1 }"
+                                        >
+                                            <div class="step-marker">1</div>
+                                        </li>
+                                        <li
+                                            class="step-item"
+                                            :class="{ 'is-completed': formData.step > 2, 'is-active': formData.step == 2 }"
+                                        >
+                                            <div class="step-marker">2</div>
+                                        </li>
+                                        <li
+                                            class="step-item"
+                                            :class="{ 'is-completed': formData.step > 3, 'is-active': formData.step == 3 }"
+                                        >
+                                            <div class="step-marker">3</div>
+                                        </li>
+                                        <li
+                                            class="step-item"
+                                            :class="{ 'is-active': formData.step == 4 }"
+                                        >
+                                            <div class="step-marker">
+                                                <i class="mdi mdi-check"></i>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
 
-                    <div class="columns">
-                        <div class="column is-full">
-                            <ul class="steps">
-                                <li
-                                    class="step-item"
-                                    :class="{ 'is-completed': formData.step > 1, 'is-active': formData.step == 1 }"
-                                >
-                                    <div class="step-marker">1</div>
-                                </li>
-                                <li
-                                    class="step-item"
-                                    :class="{ 'is-completed': formData.step > 2, 'is-active': formData.step == 2 }"
-                                >
-                                    <div class="step-marker">2</div>
-                                </li>
-                                <li
-                                    class="step-item"
-                                    :class="{ 'is-completed': formData.step > 3, 'is-active': formData.step == 3 }"
-                                >
-                                    <div class="step-marker">3</div>
-                                </li>
-                                <li
-                                    class="step-item"
-                                    :class="{ 'is-active': formData.step == 4 }"
-                                >
-                                    <div class="step-marker">
-                                        <i class="mdi mdi-check"></i>
+                            <div class="columns mt-5 mb-3">
+                                <div class="column is-10 is-offset-1">
+                                    <div v-show="formData.step == 1">
+                                        <b-field
+                                            :type="getFieldType()"
+                                            :message="getFieldMessage('name')"
+                                        >
+                                            <b-input
+                                                placeholder="Name *"
+                                                rounded
+                                                v-model="formData.name"
+                                                @keyup.native.enter="validateStep"
+                                            ></b-input>
+                                        </b-field>
                                     </div>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
 
-                    <div class="columns mt-5 mb-3">
-                        <div class="column is-10 is-offset-1">
-                            <section v-show="formData.step == 1">
-                                <b-field
-                                    :type="Object.keys(validation).length > 0 ? 'is-danger' : '' "
-                                    :message="validation.hasOwnProperty('name') ? validation.name[0] : '' "
-                                >
-                                    <b-input
-                                        placeholder="Name *"
+                                    <div v-show="formData.step == 2">
+                                        <b-field
+                                            :type="getFieldType()"
+                                            :message="getFieldMessage('surname')"
+                                        >
+                                            <b-input
+                                                placeholder="Surname *"
+                                                rounded
+                                                v-model="formData.surname"
+                                                @keyup.native.enter="validateStep"
+                                            ></b-input>
+                                        </b-field>
+                                    </div>
+
+                                    <div v-show="formData.step == 3">
+                                        <b-field
+                                            :type="getFieldType()"
+                                            :message="getFieldMessage('email')"
+                                        >
+                                            <b-input
+                                                placeholder="Email *"
+                                                rounded
+                                                v-model="formData.email"
+                                                @keyup.native.enter="validateStep"
+                                            ></b-input>
+                                        </b-field>
+                                    </div>
+
+                                    <div v-show="formData.step == 4" class="has-text-centered min-height-success">
+                                        <p class="has-text-weight-bold">Success!</p>
+                                        <p>Thank you for filling out the form</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="columns">
+                                <div class="column is-10 is-offset-1">
+                                    <b-button
+                                        type="is-primary is-light"
                                         rounded
-                                        v-model="formData.name"
-                                        @keyup.native.enter="validateStep"
-                                    ></b-input>
-                                </b-field>
-                            </section>
+                                        @click="reset"
+                                        v-if="formData.step == 4"
+                                    >
+                                        Restart?
+                                    </b-button>
 
-                            <section v-show="formData.step == 2">
-                                <b-field
-                                    :type="Object.keys(validation).length > 0 ? 'is-danger' : '' "
-                                    :message="validation.hasOwnProperty('surname') ? validation.surname[0] : '' "
-                                >
-                                    <b-input
-                                        placeholder="Surname *"
+                                    <b-button
+                                        type="is-primary is-light"
                                         rounded
-                                        v-model="formData.surname"
-                                        @keyup.native.enter="validateStep"
-                                    ></b-input>
-                                </b-field>
-                            </section>
+                                        @click="stepBack"
+                                        :disabled="formData.step == 1"
+                                        v-else
+                                    >
+                                        Prev
+                                    </b-button>
 
-                            <section v-show="formData.step == 3">
-                                <b-field
-                                    :type="Object.keys(validation).length > 0 ? 'is-danger' : '' "
-                                    :message="validation.hasOwnProperty('email') ? validation.email[0] : '' "
-                                >
-                                    <b-input
-                                        placeholder="Email *"
+                                    <b-button
+                                        type="is-primary"
+                                        class="is-pulled-right"
                                         rounded
-                                        v-model="formData.email"
-                                        @keyup.native.enter="validateStep"
-                                    ></b-input>
-                                </b-field>
-                            </section>
-
-                            <section v-show="formData.step == 4" class="has-text-centered min-height-success">
-                                <p class="has-text-weight-bold">Success!</p>
-                                <p>Thank you for filling out the form</p>
-                            </section>
+                                        @click="validateStep"
+                                        v-if="formData.step < 4"
+                                    >
+                                        Next
+                                    </b-button>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-
-                    <div class="columns">
-                        <div class="column is-10 is-offset-1">
-                            <b-button
-                                type="is-primary is-light"
-                                rounded
-                                @click="reset"
-                                v-if="formData.step == 4"
-                            >
-                                Restart?
-                            </b-button>
-
-                             <b-button
-                                type="is-primary is-light"
-                                rounded
-                                @click="stepBack"
-                                :disabled="formData.step == 1"
-                                v-else
-                            >
-                                Prev
-                            </b-button>
-
-                            <b-button
-                                type="is-primary"
-                                class="is-pulled-right"
-                                rounded
-                                @click="validateStep"
-                                v-if="formData.step < 4"
-                            >
-                                Next
-                            </b-button>
-                        </div>
-                    </div>
-
+                    </transition>
                 </div>
             </div>
         </div>
@@ -147,37 +136,77 @@
     export default {
         data() {
             return {
+                loading: false,
+                transitionType: "fade",
+                windowWidth: window.innerWidth,
                 formData: {
                     step: 1,
                     name: '',
                     surname: '',
                     email: ''
                 },
-                validation: {}
+                validation: {},
             }
         },
+        mounted() {
+            this.$nextTick(() => {
+                window.addEventListener('resize', this.onResize);
+            })
+        },
+        beforeDestroy() {
+            window.removeEventListener('resize', this.onResize);
+        },
         methods: {
+            onResize() {
+                this.windowWidth = window.innerWidth;
+            },
+            getFieldType(){
+                return Object.keys(this.validation).length > 0 ? 'is-danger' : '';
+            },
+            getFieldMessage(field){
+                return this.validation.hasOwnProperty(field) ? this.validation.[field][0] : '' ;
+            },
+            toggleLoading(){
+                if(this.windowWidth < 768)
+                    this.loading = !this.loading;
+            },
             validateStep(){
+                this.toggleLoading();
+
                 axios.post('/api/validate-step', this.formData)
                 .then((res) => {
-                    this.formData.step = res.data.step;
-                    this.validation = res.data.validation;
+                    this.transitionType = Object.keys(res.data.validation).length == 0 ? "slide-prev" : "fade"
+
+                    setTimeout(() => {
+                        this.validation = res.data.validation;
+                        this.formData.step = res.data.step;
+                        this.toggleLoading()
+                    }, 100);
                 })
                 .catch(error => {
                     console.log(error);
                 })
             },
             stepBack(){
-                if(this.formData.step == 2)
-                    this.formData.surname = '';
-                if(this.formData.step == 3)
-                    this.formData.email = '';
+                this.toggleLoading();
 
-                this.validation = {};
-                this.formData.step--;
+                this.transitionType = "slide-next";
+
+                setTimeout(() => {
+                    if(this.formData.step == 2)
+                        this.formData.surname = '';
+                    if(this.formData.step == 3)
+                        this.formData.email = '';
+
+                    this.validation = {};
+                    this.formData.step--;
+
+                    this.toggleLoading()
+                }, 100);
             },
             reset() {
-                Object.assign(this.$data, this.$options.data.call(this));
+                this.toggleLoading();
+                setTimeout(() => Object.assign(this.$data, this.$options.data.call(this)), 100);
             }
         }
     }
--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -5,7 +5,31 @@
         <div class="hero-body">
             <div class="container">
                 <div id="app">
-                    <form-component></form-component>
+                    <div class="columns">
+                        <div class="column is-6 is-offset-3">
+                            <div class="box">
+
+                                <div class="columns">
+                                    <div class="column is-full">
+                                        <h1 class="title is-4 has-text-primary has-text-centered">
+                                            3 Steps Form
+                                        </h1>
+                                    </div>
+                                </div>
+
+                                <div class="columns">
+                                    <div class="column is-full p-0">
+                                        <h3 class="subtitle is-6 has-text-grey-darker has-text-centered p-0">
+                                            Practice Test | Treedom
+                                        </h3>
+                                    </div>
+                                </div>
+
+                                <form-component></form-component>
+
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
